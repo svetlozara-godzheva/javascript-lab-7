3e4e6f480a2424e56897a0dc662a89bb26fa7f58
svetlozara-godzheva <n01754024@humber.ca>
2025-07-26 11:43:35 -0400

    Added keyboard event to an input

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ var mouseEventsText = document.getElementById("mouse-events-message");
 mouseEventsButton.addEventListener("mouseover", () => {
     mouseEventsText.innerHTML = "Hovered button";
 });
+
 mouseEventsButton.addEventListener("mouseleave", () => {
     mouseEventsText.innerHTML = "Mouse left the button";
+});
+
+var inputKeyboardEvents = document.getElementById("input-keyboard-events");
+var keyEventsText = document.getElementById("keyboard-events-meassage");
+inputKeyboardEvents.addEventListener("keydown", (e) => {
+    keyEventsText.innerHTML = `Key pressed: ${e.key}`;
 });
